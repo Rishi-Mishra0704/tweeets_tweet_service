97b5f75bd66aba06984ef02563c73ab8d1cf54f0
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-22 01:18:25 +0530

    add model to admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Tweet, Like, Comment
+from .models import Tweet, Like, Comment, Follow
 # Register your models here.
 admin.site.register(Tweet)
 admin.site.register(Like)
+admin.site.register(Follow)
 admin.site.register(Comment)
