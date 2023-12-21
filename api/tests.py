197e1696ea691d2850a3d1863a4e2dbd3a0f4ebe
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-21 23:02:24 +0530

    wrtite tests for comment api and update create tweet test

--- api/tests.py
+++ api/tests.py
@@ -3,13 +3,15 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 from django.contrib.auth.models import User
-from .models import Tweet
+from .models import Tweet, Comment
 
 class TweetAPITest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='testpassword')
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
+
+        self.tweet = Tweet.objects.create(title='Test Tweet', content='This is a test tweet', user=self.user)
 
     def test_create_tweet(self):
         url = reverse('create_tweet_api')
@@ -18,8 +20,8 @@ class TweetAPITest(TestCase):
         response = self.client.post(url, data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Tweet.objects.count(), 1)
-        self.assertEqual(Tweet.objects.get().title, 'New Tweet')
+        self.assertEqual(Tweet.objects.count(), 2) 
+        self.assertEqual(Tweet.objects.last().title, 'New Tweet')
 
 
     def test_get_tweets(self):
@@ -30,3 +32,22 @@ class TweetAPITest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('tweets', response.data)
         self.assertEqual(len(response.data['tweets']), Tweet.objects.count())
+
+    def test_comment_tweet(self):
+        url = reverse('comment_tweet', args=[self.tweet.id])
+        data = {'content': 'This is a comment on the tweet.'}
+
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Comment.objects.count(), 1)
+        self.assertEqual(Comment.objects.get().content, 'This is a comment on the tweet.')
+
+    def test_comment_tweet_not_found(self):
+        url = reverse('comment_tweet', args=[999]) 
+        data = {'content': 'This is a comment on a non-existent tweet.'}
+
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(Comment.objects.count(), 0)
