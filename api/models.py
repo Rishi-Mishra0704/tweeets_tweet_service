ef9f7790ea1ef03b823fd573b68fb9f1f2c0e489
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-21 21:05:33 +0530

    add login and registration

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tweet(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} by {self.user.username}"
--- core/forms.py
+++ core/forms.py
@@ -2,14 +2,41 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
-class UserRegistrationForm(UserCreationForm):
-    email = forms.EmailField()
 
+class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ("username", "email", "password1", "password2")
+
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Your username', 'class': 'w-full px-4 py-6 rounded-xl'})
+    )
+
+    email = forms.EmailField(
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Your email', 'class': 'w-full px-4 py-6 rounded-xl'})
+    )
+
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={'placeholder': 'Your password', 'class': 'w-full px-4 py-6 rounded-xl'})
+    )
+
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={'placeholder': 'Confirm your password', 'class': 'w-full px-4 py-6 rounded-xl'})
+    )
+
 
 class UserLoginForm(AuthenticationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'password']
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Your username', 'class': 'w-full px-4 py-6 rounded-xl'})
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={'placeholder': 'Your password', 'class': 'w-full px-4 py-6 rounded-xl'})
+    )
+
