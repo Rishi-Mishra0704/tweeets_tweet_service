2e76601bad7189f8f41a88439f62a605bd93b9cd
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-21 20:19:37 +0530

    add templates and login form

--- tweeets_tweet_service/settings.py
+++ tweeets_tweet_service/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
+    'core',
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,9 @@ ROOT_URLCONF = 'tweeets_tweet_service.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "./templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
