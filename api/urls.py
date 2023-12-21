fdd9fc22a4a8c11cb30db9c7609a8c07158d134c
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-21 22:54:34 +0530

    add comments endpoint

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('tweets/', views.tweets_api, name='tweets_api'),
     path('tweets/create/', views.create_tweet_api, name='create_tweet_api'),
+    path('tweets/<int:tweet_id>/comment/', views.comment_tweet, name='comment_tweet'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
+from rest_framework import status
+
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from .models import Tweet, Like, Comment
-from .serializers import TweetSerializer
+from .serializers import TweetSerializer, CommentSerializer, LikeSerializer
 @api_view(['GET'])
 def tweets_api(request):
     tweets = Tweet.objects.all()
@@ -20,3 +22,17 @@ def create_tweet_api(request):
         serializer.save(user=request.user)
         return Response({'status': 'Tweet created'}, status=201)
     return Response(serializer.errors, status=400)
+
+@api_view(['POST'])
+def comment_tweet(request, tweet_id):
+    try:
+        tweet = Tweet.objects.get(pk=tweet_id)
+    except Tweet.DoesNotExist:
+        return Response({'detail': 'Tweet not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = CommentSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(user=request.user, tweet=tweet)
+        return Response({'detail': 'Comment added successfully.'}, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
