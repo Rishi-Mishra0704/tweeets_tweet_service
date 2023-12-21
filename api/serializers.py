eee3eee24f2de7853c0590db070bde72fc966ac3
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-21 21:50:59 +0530

    update serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,23 +1,18 @@
 from rest_framework import serializers
-from models import Tweet, Like, Comment
-
-
-class TweetSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Tweet
-        fields = '__all__'
-
-
-class LikeSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Like
-        fields = '__all__'
-
+from .models import Tweet, Like, Comment
 
 class CommentSerializer(serializers.ModelSerializer):
-    
-        class Meta:
-            model = Comment
-            fields = '__all__'
+    class Meta:
+        model = Comment
+        fields = ['user__username', 'content']
+
+class TweetSerializer(serializers.ModelSerializer):
+    likes_count = serializers.SerializerMethodField()
+    comments = CommentSerializer(many=True)
+
+    class Meta:
+        model = Tweet
+        fields = ['id', 'title', 'content', 'user__username', 'likes_count', 'comments']
+
+    def get_likes_count(self, obj):
+        return Like.objects.filter(tweet=obj).count()
